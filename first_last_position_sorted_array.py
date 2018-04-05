c222a19a1f3a024c755d5dabb351f499ea98f9c7
Yuriy Bash <yuriybash@gmail.com>
2018-04-05 15:41:44 -0400

    clean up first_last_position

--- first_last_position_sorted_array.py
+++ first_last_position_sorted_array.py
@@ -3,7 +3,6 @@ class Solution(object):
 
         def bin_search(arr, elem, start=0, end=None):
 
-
             if end is None:
                 end = len(arr)-1
             if start > end:
@@ -17,37 +16,31 @@ class Solution(object):
             elif elem < arr[midpoint]:
                 return bin_search(arr, elem, start=start, end=midpoint-1)
 
+        def find_left_or_right_position(arr, left_most):
+            most = -1
+            start = 0
+            end = None
 
-        left_most = -1
-        start = 0
-        end = None
+            while True:
+                idx_found = bin_search(nums, target, start=start, end=end)
+                if idx_found != -1:
+                    most = idx_found
 
-        while True:
-            idx_found = bin_search(nums, target, start=start, end=end)
-            if idx_found != -1:
-                left_most = idx_found
-                start = 0
-                end = idx_found-1
-            else:
-                break
+                    if left_most == 1:
+                        start = 0
+                        end = idx_found - 1
+                    else:
+                        start = idx_found + 1
+                        end = None
+                else:
+                    break
 
-                left_most = -1
+            return most
 
-        if left_most == -1:
-            return [-1, -1]
-
-
-        right_most = -1
-        start = 0
-        end = None
-
-        while True:
-            idx_found = bin_search(nums, target, start=start, end=end)
-            if idx_found != -1:
-                right_most = idx_found
-                start = idx_found+1
-                end = None
-            else:
-                break
+        left_most = find_left_or_right_position(nums, 1)
+        right_most = find_left_or_right_position(nums, 0)
 
         return [left_most, right_most]
+
+print Solution().searchRange([5, 7,7,8,8,10], 8)
+print Solution().searchRange([2, 2], 2)
