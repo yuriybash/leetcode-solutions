00313e82e57345241803cc57747eff30cb1ad7b8
Yuriy Bash <yuriybash@gmail.com>
2018-07-25 19:17:26 -0400

    cleans up max_points_on_line

--- max_points_on_line.py
+++ max_points_on_line.py
@@ -1,5 +1,9 @@
+"""
+Given n points on a 2D plane, find the maximum number of points that lie on 
+the same straight line.
+"""
+
 from fractions import Fraction
-from itertools import combinations, chain
 
 
 # Definition for a point.
@@ -31,7 +35,6 @@ class Solution(object):
         for i in range(len(unique_pts) - 1):
 
             first_pt, second_pt = unique_pts[i], unique_pts[i + 1]
-
             pts_on_line = 2
             for pt in unique_pts[0:i] + unique_pts[i + 2:]:
 
@@ -46,6 +49,21 @@ class Solution(object):
         return max_pts_on_line + num_dupes
 
     def _point_on_line(self, fixed_pt_1, fixed_pt_2, test_pt, vertical=False):
+        """
+        Given a line defined by two fixed points, determine whether the third
+        point is on that line.
+
+        :param fixed_pt_1: first fixed point
+        :type fixed_pt_1: Point
+        :param fixed_pt_2: second fixed point
+        :type fixed_pt_2: Point
+        :param test_pt: the test point
+        :type test_pt: Point
+        :param vertical: whether the line is vertical (needed to avoid div. 0)
+        :type vertical: bool
+        :return: whether the test point is on the line
+        :rtype: bool
+        """
 
         if vertical:
             return test_pt.x == fixed_pt_1.x
@@ -54,44 +72,3 @@ class Solution(object):
         b = fixed_pt_1.y - m * fixed_pt_1.x
         if (test_pt.y == m * test_pt.x + b):
             return True
-
-
-assert(Solution().maxPoints([Point(1, 1), Point(2, 2), Point(3, 3)]) == 3)
-
-assert((Solution().maxPoints([
-    Point(1, 1),
-    Point(3, 2),
-    Point(5, 3),
-    Point(4, 1),
-    Point(2, 3),
-    Point(1, 4)])) == 4)
-
-assert(Solution().maxPoints([Point(4, 0), Point(4, -1), Point(4, 5)]) == 3)
-
-assert(Solution().maxPoints([Point(0, 0), Point(1, 1), Point(1, -1)]) == 2)
-
-assert(Solution().maxPoints([Point(1, 1), Point(1, 1), Point(2, 3)]) == 3)
-
-assert(Solution().maxPoints([Point(0, 0), Point(1, 1), Point(0, 0)]) == 3)
-
-assert(Solution().maxPoints([Point(1, 1), Point(1, 1), Point(1, 1)]) == 3)
-
-assert(Solution().maxPoints([Point(1, 1), Point(2, 2), Point(3, 3)]) == 3)
-
-assert((Solution().maxPoints([
-    Point(1, 1),
-    Point(3, 2),
-    Point(5, 3),
-    Point(4, 1),
-    Point(2, 3),
-    Point(1, 4)])) == 4)
-
-assert(Solution().maxPoints([Point(4, 0), Point(4, -1), Point(4, 5)]) == 3)
-
-assert(Solution().maxPoints([Point(0, 0), Point(1, 1), Point(1, -1)]) == 2)
-
-assert(Solution().maxPoints([Point(1, 1), Point(1, 1), Point(2, 3)]) == 3)
-
-assert(Solution().maxPoints([Point(0, 0), Point(1, 1), Point(0, 0)]) == 3)
-
-assert(Solution().maxPoints([Point(1, 1), Point(1, 1), Point(1, 1)]) == 3)
