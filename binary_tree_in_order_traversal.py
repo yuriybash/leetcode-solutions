e5ab17aec8d2e2497789b337681dc4289ca4c889
Yuriy Bash <yuriybash@gmail.com>
2018-07-27 15:55:57 -0400

    adds iterative solution to binary tree in order solution

--- binary_tree_in_order_traversal.py
+++ binary_tree_in_order_traversal.py
@@ -9,10 +9,13 @@ class TreeNode(object):
         self.left = None
         self.right = None
 
+    def __repr__(self):
+        return "TreeNode <{}>".format(self.val)
+
 class Solution(object):
-    def in_order_traversal(self, root):
+    def in_order_traversal_rcrs(self, root):
         """
-        in-order traversal of binary tree
+        in-order traversal of binary tree (recursive version)
 
         :param root: root node
         :type root: TreeNode
@@ -23,9 +26,38 @@ class Solution(object):
             return node_vals
 
         if(root.left):
-            node_vals.extend(self.in_order_traversal(root.left))
+            node_vals.extend(self.in_order_traversal_rcrs(root.left))
         node_vals.append(root.val)
         if(root.right):
-            node_vals.extend(self.in_order_traversal(root.right))
+            node_vals.extend(self.in_order_traversal_rcrs(root.right))
 
         return node_vals
+
+    def in_order_traversal_iter(self, root):
+        """
+        in-order traversal of binary tree (iterative version)
+
+        :param root: the tree (referenced by root)
+        :type root: TreeNode
+        :return: in-order traversal (of node values)
+        :rtype: [int]
+        """
+
+        final_order, stack = [], []
+        curr_node = root
+        done = False
+
+        while(not done):
+            if curr_node.left:
+                stack.append(curr_node)
+                curr_node = curr_node.left
+            else:
+                final_order.append(curr_node.val)
+                if stack:
+                    curr_node = stack.pop()
+                    final_order.append(curr_node.val)
+                    curr_node = curr_node.right
+                else:
+                    done = True
+
+        return final_order
