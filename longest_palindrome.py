d9374864c156df13b1f3c8d30bd4a9676379d833
Yuriy Bash <yuriybash@gmail.com>
2018-08-10 12:48:34 -0400

    adds longest_palindrome

---
+++ longest_palindrome.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+
+        if len(s) <= 1:
+            return s
+
+        # shortcut, executed quickly by cpython
+        if len(s) * s[0] == s:
+            return s
+
+        longest = s[0]
+        for idx, char in enumerate(s):
+            for subsqnt_char in s[idx+1:]:
+                char += subsqnt_char
+                if char == char[::-1] and len(char) > len(longest):
+                    longest = char
+        return longest
